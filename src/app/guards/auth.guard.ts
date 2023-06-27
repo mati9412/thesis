b102fd75ee7d286335ad7137b54bf017506455fe
Mateusz Przesmycki <m_przesmycki@ms.wwsi.edu.pl>
2023-06-27 17:31:08 +0200

    Final version 27.06

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -14,7 +14,6 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService) {}
   canActivate() {
-    // return this.auth.isAuthenticated;
-    return true;
+    return this.auth.isAuthenticated;
   }
 }
